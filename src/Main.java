3c49975bbd093a3f8b2d8e9766a5e7e7455a39c7
sanada_sanjiro <trollegon@narod.ru>
2026-02-18 09:40:03 +0800

    Finaly done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int diskNumber = 5;
-        Peg peg1 = new Peg(diskNumber, 'A');
+        int diskNumber; // количество дисков, которые нужно переместить между стержнями
+        if (args.length> 0) {
+            diskNumber = Integer.getInteger(args[0]);
+        } else {
+            diskNumber = 5;
+        }
+
+        Peg peg1 = new Peg(diskNumber, 'A'); // Создаем стержень с заданным числом дисков и два пустых
         Peg peg2 = new Peg(0, 'B');
         Peg peg3 = new Peg(0, 'C');
-        PegPrinter pg = new ConsolePegPrinter(peg1, peg2, peg3);
-        pg.draw();
+
+        PegPrinter pg = new ConsolePegPrinter(peg1, peg2, peg3); // Создаем объект для отображения состояния стойки
+
         RecursiveDiskMover mover = new RecursiveDiskMover(pg);
-        mover.moveDisks(peg1, peg3, peg2, diskNumber);
+        mover.moveDisks(peg1, peg3, peg2, diskNumber); // Запускаем рекурсивный метод для перемещения дисков между стержнями
     }
 }
--- src/RecursiveDiskMover.java
+++ src/RecursiveDiskMover.java
@@ -1,19 +1,30 @@
+/**
+ * Класс, предоставляющий рекурсивный метод для решения головоломки про ханойские башенки
+ */
 public class RecursiveDiskMover {
-    private final PegPrinter printer;
+    private final PegPrinter printer;  // объект, отображающий состояние стержней и дисков на каждом шаге в консоли
 
     public RecursiveDiskMover(PegPrinter printer) {
         this.printer = printer;
     }
 
+    /**
+     * Рекурсивный метод, перемещающий диски между заданными стержнями
+     * @param from Peg стержень, на котором помещены диски
+     * @param to Peg стержень, на которые нужно перенести диски
+     * @param aux Peg вспомогательный стержень
+     * @param n число дисков, которые нужно переместить. При первоначальном запуске равно количеству дисков на первом стержне
+     */
     public void moveDisks(Peg from, Peg to, Peg aux, int n) {
+        // Если нужно переместить 1 диск - просто перекладываем его на нужный стержень
         if (n==1) {
             Disk disk = from.pop();
             to.push(disk);
             printer.draw();
-        } else {
-            moveDisks(from, aux, to, n - 1);
-            moveDisks(from, to, aux, 1);
-            moveDisks(aux, to, from, n - 1);
+        } else {                                    // Если перемещается больше одного диска
+            moveDisks(from, aux, to, n - 1);  // переносим n-1 диск на вспомогательный стержень
+            moveDisks(from, to, aux, 1);      // переносим 1 диск на конечный стержень
+            moveDisks(aux, to, from, n - 1);  // переносим диски, помещенные ранее на вспомогательный стержень, на конечный
         }
     }
 }
